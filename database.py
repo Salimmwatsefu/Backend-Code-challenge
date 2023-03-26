9b4fecca2a4af81fff766582fff418d33aafff7b
Salim <sjmwatsefu@gmail.com>
2023-03-26 08:46:44 +0300

    create questions table

--- database.py
+++ database.py
@@ -9,3 +9,10 @@ id INTEGER PRIMARY KEY AUTOINCREMENT,
 username TEXT NOT NULL UNIQUE,
 password TEXT NOT NULL
 )''')
+
+cursor.execute(''' CREATE TABLE questions(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+body TEXT NOT NULL,
+author_id INTEGER NOT NULL,
+FOREIGN KEY (author_id) REFERENCES users(id)
+)''')
