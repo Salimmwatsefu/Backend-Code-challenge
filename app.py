79f7539cd1708d33c3b367099a8a88b437268f84
Salim <sjmwatsefu@gmail.com>
2023-03-26 10:35:13 +0300

    login user endpoint

--- app.py
+++ app.py
@@ -31,3 +31,23 @@ def register():
     return jsonify({"message": "User registered successfully"})
 
 
+#Log a user in
+
+@app.route('/auth/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, password FROM users WHERE username=?", (username,))
+    user = cursor.fetchone()
+
+    # Check if the user exists and the password is correct
+    if user and check_password_hash(user[1], password):
+        # Create a session for the user by storing the user's ID in a cookie
+        response = jsonify({"message": "User logged in successfully."})
+        response.set_cookie('user_id', str(user[0]))
+        return response
+    else:
+        return jsonify({"error": "Invalid username or password."})
