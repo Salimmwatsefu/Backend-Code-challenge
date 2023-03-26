6277aac5c74fd14188f71113274e58a52de5d300
Salim <sjmwatsefu@gmail.com>
2023-03-26 08:35:15 +0300

    create users table

--- database.py
+++ database.py
@@ -3,3 +3,9 @@ import sqlite3
 conn = sqlite3.connect('database.db', check_same_thread=False)
 
 cursor = conn.cursor()
+
+cursor.execute(''' CREATE TABLE users(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT NOT NULL UNIQUE,
+password TEXT NOT NULL
+)''')
